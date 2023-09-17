6d456220074e25f6e58e958471acf240886cce3e
Marc Lally <m.lally@pm.me>
2023-09-17 13:17:27 +0100

    configure notie for contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -259,67 +259,21 @@
       }
     }).then(response => {
       if (response.ok) {
-        toastSuccess
+        // status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
         form.reset()
       } else {
         response.json().then(data => {
           if (Object.hasOwn(data, 'errors')) {
             // status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-            toastFailure
           } else {
             // status.innerHTML = "Oops! There was a problem submitting your form"
-            toastFailure
           }
         })
       }
     }).catch(error => {
-      toastFailure
       // // status.innerHTML = "Oops! There was a problem submitting your form"
       // status.innerHTML = "Oops! There was a problem submitting your form"
     });
   }
   form.addEventListener("submit", handleSubmit)
-
-  // Toast
-  var toastSuccess =
-    $(document).ready(function () {
-      $.toast({
-        text: "Message has been sent!", // Text that is to be shown in the toast
-        heading: 'Success', // Optional heading to be shown on the toast
-        icon: 'success', // Type of toast icon
-        showHideTransition: 'fade', // fade, slide or plain
-        allowToastClose: true, // Boolean value true or false
-        hideAfter: 5000, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
-        stack: 5, // false if there should be only one toast at a time or a number representing the maximum number of toasts to be shown at a time
-        position: 'bottom-left', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values      
-        textAlign: 'left',  // Text alignment i.e. left, right or center
-        loader: true,  // Whether to show loader or not. True by default
-        loaderBg: '#EF6024',  // Background color of the toast loader
-        beforeShow: function () { }, // will be triggered before the toast is shown
-        afterShown: function () { }, // will be triggered after the toat has been shown
-        beforeHide: function () { }, // will be triggered before the toast gets hidden
-        afterHidden: function () { }  // will be triggered after the toast has been hidden
-      });
-    });
-
-  var toastFailure =
-    $(document).ready(function () {
-      $.toast({
-        text: "Something has gone wrong, there has been an error. Please try again", // Text that is to be shown in the toast
-        heading: 'Failure!', // Optional heading to be shown on the toast
-        icon: 'error', // Type of toast icon
-        showHideTransition: 'fade', // fade, slide or plain
-        allowToastClose: true, // Boolean value true or false
-        hideAfter: 5000, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
-        stack: 5, // false if there should be only one toast at a time or a number representing the maximum number of toasts to be shown at a time
-        position: 'bottom-left', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values      
-        textAlign: 'left',  // Text alignment i.e. left, right or center
-        loader: true,  // Whether to show loader or not. True by default
-        loaderBg: '#EF6024',  // Background color of the toast loader
-        beforeShow: function () { }, // will be triggered before the toast is shown
-        afterShown: function () { }, // will be triggered after the toat has been shown
-        beforeHide: function () { }, // will be triggered before the toast gets hidden
-        afterHidden: function () { }  // will be triggered after the toast has been hidden
-      });
-    });
 })()
