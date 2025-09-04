f6d8f10854712b3c4609d7c36758231e7bdc85fe
Marc Lally <marc.lally@gmail.com>
2025-09-04 20:08:11 +0100

    initial commit.

---
+++ assets/js/main.js
@@ -0,0 +1,221 @@
+(function () {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  /**
+   * Mobile nav toggle
+   */
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+  });
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on(
+    "click",
+    "#navbar .nav-link",
+    function (e) {
+      let section = select(this.hash);
+      if (section) {
+        e.preventDefault();
+        let blockquote = select("#block");
+        let navbar = select("#navbar");
+        let header = select("#header");
+        let sections = select("section", true);
+        let navlinks = select("#navbar .nav-link", true);
+
+        if (blockquote) {
+          blockquote.remove();
+        };
+
+        navlinks.forEach((item) => {
+          item.classList.remove("active");
+        });
+
+        this.classList.add("active");
+
+        if (navbar.classList.contains("navbar-mobile")) {
+          navbar.classList.remove("navbar-mobile");
+          let navbarToggle = select(".mobile-nav-toggle");
+          navbarToggle.classList.toggle("bi-list");
+          navbarToggle.classList.toggle("bi-x");
+        }
+
+        if (this.hash == "#header") {
+          header.classList.remove("header-top");
+          sections.forEach((item) => {
+            item.classList.remove("section-show");
+          });
+          return;
+        }
+
+        if (!header.classList.contains("header-top")) {
+          header.classList.add("header-top");
+          setTimeout(function () {
+            sections.forEach((item) => {
+              item.classList.remove("section-show");
+            });
+            section.classList.add("section-show");
+          }, 350);
+        } else {
+          sections.forEach((item) => {
+            item.classList.remove("section-show");
+          });
+          section.classList.add("section-show");
+        }
+
+        scrollto(this.hash);
+      }
+    },
+    true
+  );
+
+  /**
+   * Activate/show sections on load with hash links
+   */
+  window.addEventListener("load", () => {
+    if (window.location.hash) {
+      let initial_nav = select(window.location.hash);
+
+      if (initial_nav) {
+        let header = select("#header");
+        let navlinks = select("#navbar .nav-link", true);
+        header.classList.add("header-top");
+
+        navlinks.forEach((item) => {
+          if (item.getAttribute("href") == window.location.hash) {
+            console.log(block);
+            item.classList.add("active");
+          } else {
+            item.classList.remove("active");
+          }
+        });
+
+        setTimeout(function () {
+          initial_nav.classList.add("section-show");
+        }, 350);
+
+        scrollto(window.location.hash);
+      }
+    }
+  });
+
+  /**
+  * Skills animation
+  */
+  let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function (direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
+
+  /**
+   * Initiate Pure Counter 
+   */
+  new PureCounter();
+
+  // Handle the form
+  var form = document.getElementById("my-form");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          notie.alert({
+            type: 1,
+            text: "Thankyou! Your message has been sent.",
+            position: "bottom",
+            time: 6,
+          });
+          form.reset();
+        } else {
+          response.json().then((data) => {
+            if (Object.hasOwn(data, "errors")) {
+              notie.alert({
+                type: 1,
+                text: "Sorry, something has gone wrong, please try again. Alternatively, schedule a meeting with me from the home page or send me an email.",
+                position: "bottom",
+                time: 10,
+              });
+            } else {
+              notie.alert({
+                type: 1,
+                text: "Sorry, something has gone wrong, please try again. Alternatively, schedule a meeting with me from the home page or send me an email.",
+                position: "bottom",
+                time: 10,
+              });
+            }
+          });
+        }
+      })
+      .catch((error) => {
+        notie.alert({
+          type: 1,
+          text: "Sorry, something has gone wrong, please try again. Alternatively, schedule a meeting with me from the home page or send me an email.",
+          position: "bottom",
+          time: 10,
+        });
+      });
+  }
+  form.addEventListener("submit", handleSubmit);
+})();
+
+$('.resumer-item > li > strong').click(function () {
+  $(this).siblings('ul').slideToggle();
+});
